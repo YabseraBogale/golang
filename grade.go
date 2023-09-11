c69baa2acb84e9908af0d2eaa48055495b337963
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-09-11 10:30:36 +0300

    Understand the switch statememnt but which is better if or switch ?

---
+++ grade.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "fmt"
+    "strconv"
+)
+
+func main(){
+    var input string
+    fmt.Print("Enter Your Grade: ")
+    fmt.Scanf("%s",&input)
+    grade,err:=strconv.Atoi(input)
+    if err!=nil{
+        fmt.Println(err)
+    }
+    switch {
+        case grade<0: fmt.Println("Enter the grade correctly")
+        case grade>0 && grade <=30: fmt.Println("F")
+        case grade>=31 && grade<=40: fmt.Println("D")
+        case grade>=41 && grade<=70: fmt.Println("C")
+        case grade>=71 && grade<=80: fmt.Println("B")
+        case grade>=81 && grade<=100: fmt.Println("A")
+        default: fmt.Println("Are you a student ?")
+
+    }
+}
