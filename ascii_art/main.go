b46c583f255a4748beb9735d835f7677aa5d967f
YabseraBogale <yabserapython@gmail.com>
2024-07-31 17:37:27 +0300

    ok

--- ascii_art/main.go
+++ ascii_art/main.go
@@ -16,7 +16,9 @@ func main() {
 	for x := 0; x < fileimage.Bounds().Max.X; x++ {
 		for y := 0; y < fileimage.Bounds().Max.Y; y++ {
 			color := fileimage.At(x, y)
-			print(color.RGBA())
+			R, B, G, _ := color.RGBA()
+			average := 0.299*R + 0.587*G + 0.114*B
+			print(average, " ")
 		}
 		println()
 	}
