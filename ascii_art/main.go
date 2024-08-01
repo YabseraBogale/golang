30cac139859bb837bb4d6ce7661d93dfc4e4b953
YabseraBogale <yabserapython@gmail.com>
2024-08-01 17:50:19 +0300

    ok

--- ascii_art/main.go
+++ ascii_art/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"bufio"
 	"image/png"
 	"os"
 )
@@ -19,32 +19,35 @@ func main() {
 	}
 	defer file.Close()
 	fileimage, err := png.Decode(file)
-	ascii := make([][]string, fileimage.Bounds().Max.Y)
+	ascii := make([][]byte, fileimage.Bounds().Max.Y)
 	for x := 0; x < fileimage.Bounds().Max.Y; x++ {
-		ascii[x] = make([]string, fileimage.Bounds().Max.X)
+		ascii[x] = make([]byte, fileimage.Bounds().Max.X)
 		for y := 0; y < fileimage.Bounds().Max.X; y++ {
 			oldcolor := fileimage.At(x, y)
 			R, B, G, _ := oldcolor.RGBA()
 			average := r*float32(R) + g*float32(G) + b*float32(B)
 			if uint8(average) == 0 {
-				ascii[x][y] = "::"
+				ascii[x][y] = byte('0')
 			} else if uint8(average) == 255 {
-				ascii[x][y] = " "
+				ascii[x][y] = byte('1')
 			} else if uint8(average) < 50 && uint8(average) != 0 && uint8(average) != 255 {
-				ascii[x][y] = "|"
+				ascii[x][y] = byte('#')
 			} else if uint8(average) >= 50 && uint8(average) < 87 && uint8(average) != 0 && uint8(average) != 255 {
-				ascii[x][y] = "="
+				ascii[x][y] = byte('(')
 			} else if uint8(average) >= 87 && uint8(average) < 162 && uint8(average) != 0 && uint8(average) != 255 {
-				ascii[x][y] = "_"
+				ascii[x][y] = byte(')')
 			} else if uint8(average) >= 162 && uint8(average) < 209 && uint8(average) != 0 && uint8(average) != 255 {
-				ascii[x][y] = ""
+				ascii[x][y] = byte('_')
 			} else if uint8(average) >= 209 && uint8(average) < 255 && uint8(average) != 0 && uint8(average) != 255 {
-				ascii[x][y] = "."
+				ascii[x][y] = byte('$')
 			}
 
 		}
 	}
 
-	fmt.Println(ascii)
+	var buff bufio.Writer
+	for _, i := range ascii {
+		buff.WriteString(string(i))
+	}
 
 }
