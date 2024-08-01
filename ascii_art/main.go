b8bd0f3f6a4c007b5ae4db2e96f896ccadc2b61d
YabseraBogale <yabserapython@gmail.com>
2024-08-01 13:50:54 +0300

    ok

--- ascii_art/main.go
+++ ascii_art/main.go
@@ -19,21 +19,27 @@ func main() {
 	}
 	defer file.Close()
 	fileimage, err := png.Decode(file)
-	ascii := make([][]byte, fileimage.Bounds().Max.X)
-	for x := 0; x < fileimage.Bounds().Max.X; x++ {
-		ascii[x] = make([]byte, fileimage.Bounds().Max.Y)
-		for y := 0; y < fileimage.Bounds().Max.Y; y++ {
+	ascii := make([][]string, fileimage.Bounds().Max.Y)
+	for x := 0; x < fileimage.Bounds().Max.Y; x++ {
+		ascii[x] = make([]string, fileimage.Bounds().Max.X)
+		for y := 0; y < fileimage.Bounds().Max.X; y++ {
 			oldcolor := fileimage.At(x, y)
 			R, B, G, _ := oldcolor.RGBA()
 			average := r*float32(R) + g*float32(G) + b*float32(B)
 			if uint8(average) == 0 {
-				ascii[x][y] = byte('O')
+				ascii[x][y] = "::"
 			} else if uint8(average) == 255 {
-				ascii[x][y] = byte('X')
-			} else if uint8(average) < 150 && uint8(average) != 0 && uint8(average) != 255 {
-				ascii[x][y] = byte('W')
-			} else if uint8(average) > 150 && uint8(average) != 0 && uint8(average) != 255 {
-				ascii[x][y] = byte('Y')
+				ascii[x][y] = " "
+			} else if uint8(average) < 50 && uint8(average) != 0 && uint8(average) != 255 {
+				ascii[x][y] = "|"
+			} else if uint8(average) >= 50 && uint8(average) < 87 && uint8(average) != 0 && uint8(average) != 255 {
+				ascii[x][y] = "="
+			} else if uint8(average) >= 87 && uint8(average) < 162 && uint8(average) != 0 && uint8(average) != 255 {
+				ascii[x][y] = "_"
+			} else if uint8(average) >= 162 && uint8(average) < 209 && uint8(average) != 0 && uint8(average) != 255 {
+				ascii[x][y] = ""
+			} else if uint8(average) >= 209 && uint8(average) < 255 && uint8(average) != 0 && uint8(average) != 255 {
+				ascii[x][y] = "."
 			}
 
 		}
