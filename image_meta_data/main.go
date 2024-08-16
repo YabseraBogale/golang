a43aaca495360260d372a049aedd48c1b063e2ea
YabseraBogale <yabserapython@gmail.com>
2024-08-16 19:00:28 +0300

    ok

---
+++ image_meta_data/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/evanoberholster/imagemeta"
+)
+
+func main() {
+	file, err := os.Open("image.png")
+	if err != nil {
+		log.Println(err)
+	}
+	defer file.Close()
+
+	img, err := imagemeta.Decode(file)
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(img)
+}
