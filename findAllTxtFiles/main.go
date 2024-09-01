bd751e99141378b6a37417ff5d106831f623444e
YabseraBogale <yabserapython@gmail.com>
2024-09-01 11:33:34 +0300

    p

--- findAllTxtFiles/main.go
+++ findAllTxtFiles/main.go
@@ -1,19 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"os/exec"
-)
-
 func main() {
-	cmd := exec.Command("ls")
-	err := cmd.Run()
-	if err != nil {
-		fmt.Println(err)
-	}
-	out, err := cmd.Output()
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(out)
+
 }
