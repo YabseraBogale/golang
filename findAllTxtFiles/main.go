8ee3ac9a7c9eb0baf50e4580e897d8f2e7005cb7
YabseraBogale <yabserapython@gmail.com>
2024-09-03 20:38:27 +0300

    ok

--- findAllTxtFiles/main.go
+++ findAllTxtFiles/main.go
@@ -16,15 +16,16 @@ func main() {
 }
 
 func ListDir(Name string) {
+
 	list, err := os.ReadDir(Name)
 	if err != nil {
-
+		log.Println(err)
 	}
 	for _, i := range list {
 		if !i.IsDir() && strings.Contains(i.Name(), ".txt") {
-			fmt.Println(i.Info())
-		} else if i.IsDir() {
-			ListDir(i.Name())
+			fmt.Println(Name + i.Name())
+		} else if i.IsDir() == true {
+			ListDir(Name + "/" + i.Name())
 		}
 	}
 }
