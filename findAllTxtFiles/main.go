df71f30d3ea804bf12354f34a2ec5e9ac483ac25
YabseraBogale <yabserapython@gmail.com>
2024-09-03 19:53:28 +0300

    ok

--- findAllTxtFiles/main.go
+++ findAllTxtFiles/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
+)
 
+func main() {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	list, err := os.ReadDir(home)
+
+	if err != nil {
+
+	}
 }
