fc2c94909f1f550567f2cc39bfe160348a9a7dc4
YabseraBogale <yabserapython@gmail.com>
2025-01-15 18:20:25 +0300

    ok

--- landscape/main.go
+++ landscape/main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("HelloWorld", w)
+	})
+	http.ListenAndServe(":8000", nil)
 }
