5ea07b940f5daed553ed7d89dfb6cff9e534ff1d
YabseraBogale <yabserapython@gmail.com>
2025-01-16 14:49:53 +0300

    ok

--- landscape/main.go
+++ landscape/main.go
@@ -11,6 +11,7 @@ func main() {
 		t, _ := template.ParseFiles("public/index.html")
 		t.Execute(w, nil)
 	})
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	err := http.ListenAndServe(":8020", nil)
 	if err != nil {
 		log.Println(err)
