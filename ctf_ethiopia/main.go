f430a10eb1801869f1ac43b535022762c1b80d3f
YabseraBogale <yabserapython@gmail.com>
2025-01-19 19:03:49 +0300

    ok

--- ctf_ethiopia/main.go
+++ ctf_ethiopia/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+	"text/template"
+)
 
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		t, _ := template.ParseFiles("public/home.html")
+		t.Execute(w, nil)
+	})
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	err := http.ListenAndServe(":5000", nil)
+	if err != nil {
+		log.Println(err)
+	}
 }
