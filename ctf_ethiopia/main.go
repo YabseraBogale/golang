73e90d6ba651e947493418386ad164aa2936300d
YabseraBogale <yabserapython@gmail.com>
2025-01-19 19:13:30 +0300

    ok

--- ctf_ethiopia/main.go
+++ ctf_ethiopia/main.go
@@ -8,11 +8,23 @@ import (
 )
 
 func main() {
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+		t, _ := template.ParseFiles("public/login.html")
+		t.Execute(w, nil)
+	})
+
+	http.HandleFunc("/signup", func(w http.ResponseWriter, r *http.Request) {
+		t, _ := template.ParseFiles("public/signup.html")
+		t.Execute(w, nil)
+	})
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		t, _ := template.ParseFiles("public/home.html")
 		t.Execute(w, nil)
 	})
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
 	fmt.Println("Listing on http://localhost:5000")
 	err := http.ListenAndServe(":5000", nil)
 	if err != nil {
