a641adb268cb032f78c10166da2b5ddd071e8290
YabseraBogale <yabserapython@gmail.com>
2025-01-19 20:03:35 +0300

    ok

--- ctf_ethiopia/main.go
+++ ctf_ethiopia/main.go
@@ -11,13 +11,22 @@ func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
 	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
-		t, _ := template.ParseFiles("public/login.html")
-		t.Execute(w, nil)
+		if r.Method == http.MethodPost {
+
+		} else if r.Method == http.MethodGet {
+			t, _ := template.ParseFiles("public/login.html")
+			t.Execute(w, nil)
+		}
+
 	})
 
 	http.HandleFunc("/signup", func(w http.ResponseWriter, r *http.Request) {
-		t, _ := template.ParseFiles("public/signup.html")
-		t.Execute(w, nil)
+		if r.Method == http.MethodPost {
+
+		} else if r.Method == http.MethodGet {
+			t, _ := template.ParseFiles("public/signup.html")
+			t.Execute(w, nil)
+		}
 	})
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
