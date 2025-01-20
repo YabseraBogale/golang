3ae2ff0a7962eaede6b10a42975fe643e5391c72
YabseraBogale <yabserapython@gmail.com>
2025-01-20 14:48:03 +0300

    ok

--- ctf_ethiopia/main.go
+++ ctf_ethiopia/main.go
@@ -12,7 +12,9 @@ func main() {
 
 	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == http.MethodPost {
-
+			username := r.FormValue("username")
+			password := r.FormValue("password")
+			fmt.Println(username, password)
 		} else if r.Method == http.MethodGet {
 			t, _ := template.ParseFiles("public/login.html")
 			t.Execute(w, nil)
@@ -22,7 +24,16 @@ func main() {
 
 	http.HandleFunc("/signup", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == http.MethodPost {
-
+			//username := r.FormValue("username")
+			//email:=r.FormValue("email")
+			password := r.FormValue("password")
+			re_password := r.FormValue("re_password")
+			if re_password != password {
+				t, _ := template.ParseFiles("public/signup.html")
+				t.Execute(w, struct{ NotMatched bool }{NotMatched: true})
+			} else {
+				http.Redirect(w, r, "/", http.StatusSeeOther)
+			}
 		} else if r.Method == http.MethodGet {
 			t, _ := template.ParseFiles("public/signup.html")
 			t.Execute(w, nil)
