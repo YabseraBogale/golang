418a4682ba4051b4f70dcb313d6c19e7ecd57b64
YabseraBogale <yabserapython@gmail.com>
2025-03-04 17:03:29 +0300

    ok

--- question/main.go
+++ question/main.go
@@ -5,16 +5,25 @@ import (
 	"log"
 	"os"
 	"regexp"
+	"strings"
 )
 
 func main() {
-	file, err := os.ReadFile("mcqs.txt")
+	file, err := os.ReadFile("test.txt")
 	if err != nil {
 		log.Println(err)
 	}
 
 	text := string(file)
-	patter := regexp.MustCompile(`| P a g e`)
-	fmt.Println(patter.FindAllStringIndex(text, -1))
+	patter := regexp.MustCompile(`(?s)\.(.*?)(?:\n(?:[a-d]\..*?\n?)+)\s*Answer:\s*([a-d]\..*?)\nExplanation:\s*(.*?)(?:\n\n|\z)`)
+	matches := patter.FindAllStringSubmatch(text, -1)
+	if matches == nil {
+		fmt.Println("No matches found")
+		return
+	}
 
+	// Loop through all matched MCQs
+	for i, match := range matches {
+		fmt.Printf("Question %d: %s\n", i+1, strings.TrimSpace(match[1]))
+	}
 }
