94ed1e2bfc7da6361fed44e700706e9150562a7b
YabseraBogale <yabserapython@gmail.com>
2025-03-04 20:16:24 +0300

    ok

--- question/main.go
+++ question/main.go
@@ -1,29 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
-	"regexp"
-	"strings"
 )
 
 func main() {
-	file, err := os.ReadFile("test.txt")
+	_, err := os.ReadFile("question.json")
 	if err != nil {
 		log.Println(err)
 	}
 
-	text := string(file)
-	patter := regexp.MustCompile(`(?s)\.(.*?)(?:\n(?:[a-d]\..*?\n?)+)\s*Answer:\s*([a-d]\..*?)\nExplanation:\s*(.*?)(?:\n\n|\z)`)
-	matches := patter.FindAllStringSubmatch(text, -1)
-	if matches == nil {
-		fmt.Println("No matches found")
-		return
-	}
-
-	// Loop through all matched MCQs
-	for i, match := range matches {
-		fmt.Printf("Question %d: %s\n", i+1, strings.TrimSpace(match[1]))
-	}
 }
