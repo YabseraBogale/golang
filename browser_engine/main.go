3a3fafa1c70f9c7c2b828c46e936e8ccc02a85a5
YabseraBogale <yabserapython@gmail.com>
2025-03-07 15:16:46 +0300

    going but still hard

--- browser_engine/main.go
+++ browser_engine/main.go
@@ -1,9 +1,25 @@
 package main
 
 import (
-	"gitub.com/YabseraBogale/golang/browser_engine/parser"
+	"github.com/YabseraBogale/golang/browser_engine/parser"
 )
 
 func main() {
-	p := parser.Parser{}
+	p := parser.Parser{
+		Pos: 0,
+		Input: `
+			<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    </body>
+</html>
+	`,
+	}
+
 }
