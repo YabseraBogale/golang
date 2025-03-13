07038c57909b1e8809a392a6abe68192d61b1559
YabseraBogale <yabserapython@gmail.com>
2025-03-13 12:11:41 +0300

    ok

--- monkey/token/token.go
+++ monkey/token/token.go
@@ -19,6 +19,18 @@ const (
 	LET       = "LET"
 )
 
+var keyword = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIndent(ident string) TokenType {
+	if tok, ok := keyword[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
+
 type Token struct {
 	Type    TokenType
 	Literal string
