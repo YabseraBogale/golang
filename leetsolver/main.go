6d667209adfdd9b6f0d5f81b2368998ee1a49bca
YabseraBogale <yabserapython@gmail.com>
2025-04-02 17:51:19 +0300

    ok

--- leetsolver/main.go
+++ leetsolver/main.go
@@ -8,6 +8,7 @@ import (
 	"os"
 
 	"github.com/google/generative-ai-go/genai"
+	"github.com/otiai10/gosseract/v2"
 	"github.com/vova616/screenshot"
 	"google.golang.org/api/option"
 )
@@ -15,38 +16,47 @@ import (
 func main() {
 	api := os.Getenv("api")
 	ctx := context.Background()
-
+	tesract := gosseract.NewClient()
+	defer tesract.Close()
 	client, err := genai.NewClient(ctx, option.WithAPIKey(api))
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer client.Close()
-	for {
-		file, err := os.Create("image.png")
-		if err != nil {
-			log.Println(err)
-		}
-		defer file.Close()
-		screen, err := screenshot.CaptureScreen()
-		if err != nil {
-			log.Println(err)
-		}
-		err = png.Encode(file, screen)
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		question := ""
-		if question == "" {
 
-		} else {
-			model := client.GenerativeModel("gemini-2.0-flash")
-			resp, err := model.GenerateContent(ctx, genai.Text("How does AI work?"))
-			if err != nil {
-				log.Fatal(err)
-			}
-			fmt.Println(resp)
+	file, err := os.Create("image.png")
+	if err != nil {
+		log.Println(err)
+	}
+	defer file.Close()
+	screen, err := screenshot.CaptureScreen()
+	if err != nil {
+		log.Println(err)
+	}
+	err = png.Encode(file, screen)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	err = tesract.SetImage(file.Name())
+	if err != nil {
+		log.Println(err)
+	}
+	text, err := tesract.Text()
+	if err != nil {
+		log.Println(err)
+	}
+
+	question := ""
+	if question == "" {
+		fmt.Println(text)
+	} else {
+		model := client.GenerativeModel("gemini-2.0-flash")
+		resp, err := model.GenerateContent(ctx, genai.Text("How does AI work?"))
+		if err != nil {
+			log.Fatal(err)
 		}
+		fmt.Println(resp)
 	}
 
 }
