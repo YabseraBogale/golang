41e7d44f2b50bf9e9ffea54c82671f01fb8abc72
YabseraBogale <yabserapython@gmail.com>
2025-04-05 20:22:43 +0300

    ok

--- ml/function/function.go
+++ ml/function/function.go
@@ -1,13 +1,6 @@
 package function
 
-func SquareInt(x []int) []int {
-	result := []int{}
-
-	for _, i := range x {
-		result = append(result, i*i)
-	}
-	return result
-}
+import "errors"
 
 func SquareFloat64(x []float64) []float64 {
 	result := []float64{}
@@ -16,4 +9,41 @@ func SquareFloat64(x []float64) []float64 {
 		result = append(result, i*i)
 	}
 	return result
+}
+
+func MatrixSum(x []float64, y []float64) ([]float64, error) {
+	if len(x) != len(y) {
+		return []float64{}, errors.New("the matrix are not equal")
+	}
+	result := []float64{}
+	for i := 0; i < len(y); i++ {
+		result = append(result, y[i]+x[i])
+	}
+	return result, nil
+}
+
+func MatrixMinus(x []float64, y []float64) ([]float64, error) {
+	if len(x) != len(y) {
+		return []float64{}, errors.New("the matrix are not equal")
+	}
+	result := []float64{}
+	for i := 0; i < len(y); i++ {
+		result = append(result, y[i]-x[i])
+	}
+	return result, nil
+}
+
+func MatrixMultiple(x []float64, y []float64) ([]float64, error) {
+	if len(x) != len(y) {
+		return []float64{}, errors.New("the matrix are not equal")
+	}
+	result := []float64{}
+	for i := 0; i < len(y); i++ {
+		result = append(result, y[i]*x[i])
+	}
+	return result, nil
+}
+
+func DervativeFunc(f func(x []int, d float64) []int, input []int, delta float64) []int {
+	return []int{}
 }
