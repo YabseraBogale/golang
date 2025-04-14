ac81157b24706ed2f07e471dfb6cf91ae94b960c
YabseraBogale <yabserapython@gmail.com>
2025-04-14 18:48:38 +0300

    looks everything is right now to test

---
+++ esh/database.sql
@@ -0,0 +1,12 @@
+create table Job(
+    
+);
+
+create table Employee(
+    id int not null primary key,
+    firstname varchar(30) not null,
+    middlename varchar(30) not null,
+    lastname varchar(30) not null,
+    phonenumber int not null,
+    email text not null
+);
