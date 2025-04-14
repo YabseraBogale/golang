92483a6baf719c107b74dbd8e7a5165f9e26973e
YabseraBogale <yabserapython@gmail.com>
2025-04-14 18:51:11 +0300

    looks everything is right now to test

--- esh/database.sql
+++ esh/database.sql
@@ -1,5 +1,7 @@
 create table Job(
-    
+    job_id int not null primary key,
+    job_title text not null,
+    job_description text not null
 );
 
 create table Employee(
@@ -8,5 +10,6 @@ create table Employee(
     middlename varchar(30) not null,
     lastname varchar(30) not null,
     phonenumber int not null,
-    email text not null
+    email text not null,
+    job_id int references Job
 );
