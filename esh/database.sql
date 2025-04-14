f67b4e024ac294013723fb061b33a1b2604d076a
YabseraBogale <yabserapython@gmail.com>
2025-04-14 19:31:35 +0300

    looks everything is right now to test

--- esh/database.sql
+++ esh/database.sql
@@ -4,12 +4,35 @@ create table if not exists Job(
     job_description text not null
 );
 
+create table if not exists Emergency_Contact(
+    emergency_contact_id not null primary key,
+    firstname varchar(30) not null,
+    middlename varchar(30) not null,
+    lastname varchar(30) not null,
+    phonenumber varchar(30) not null,
+    email varchar(30) not null,
+    fyda_id text not null
+
+);
+
 create table if not exists Employee(
     id int not null primary key,
     firstname varchar(30) not null,
     middlename varchar(30) not null,
     lastname varchar(30) not null,
     phonenumber int not null,
+    fyda_id text not null,
     email text not null,
+    date date not null,
+    emergency_contact_id int references Emergency_Contact,
     job_id int references Job
 );
+
+create table if not exists item(
+    item_id varchar(30) not null primary key,
+    item_name varchar(30) not null,
+    item_description text not null,
+    item_quantity int not null,
+    date date not null,
+
+)
