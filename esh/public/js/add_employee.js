6c6edff7e0ea9790d399b2271ca7cc4c2ae25150
YabseraBogale <yabserapython@gmail.com>
2025-04-25 16:23:14 +0300

    ok ok ok

--- esh/public/js/add_employee.js
+++ esh/public/js/add_employee.js
@@ -1,1 +1,9 @@
-
+fetch("127.0.0.1:8080")
+    .then((response)=>{
+        return response.json()
+    })
+    .then((data)=>{
+        console.log(data)
+    }).catch(err=>{
+        console.log(err)
+    })
