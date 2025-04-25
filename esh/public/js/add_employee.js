92626c3c162db20e2dbc436061ae23f292c2679e
YabseraBogale <yabserapython@gmail.com>
2025-04-25 17:34:46 +0300

    ok ok ok

--- esh/public/js/add_employee.js
+++ esh/public/js/add_employee.js
@@ -2,7 +2,13 @@ fetch("http://127.0.0.1:8080/department").then((response)=>{
         return response.json()
     }).then(data =>{
        
-        console.log(data);
+       
+        const department=document.getElementById("department")
+        let option=""
+        for(let i=0;i<data.length;i++){
+            option+="<option value='"+data[i]+"'>"+data[i]+"</option>"
+        }
+        department.innerHTML=option
         
     }).catch(err =>{
         console.error(err)
