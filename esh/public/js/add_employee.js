546f0f7befd1a8aca0d3d70f638c8fbda7cb69d1
YabseraBogale <yabserapython@gmail.com>
2025-04-25 17:49:49 +0300

    ok ok ok

--- esh/public/js/add_employee.js
+++ esh/public/js/add_employee.js
@@ -12,4 +12,19 @@ fetch("http://127.0.0.1:8080/department").then((response)=>{
         
     }).catch(err =>{
         console.error(err)
-    })
+})
+
+fetch("http://127.0.0.1:8080/job_title").then((response)=>{
+        return response.json()
+    }).then(data =>{
+       
+       
+        const job_title=document.getElementById("job_title")
+        let option=""
+        for(let i=0;i<data.length;i++){
+            option+="<option value='"+data[i]+"'>"+data[i]+"</option>"
+        }
+        job_title.innerHTML=option
+        
+    }).catch(err =>{
+        console.error(err)})
