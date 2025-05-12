7158c164290e7d00a2ca1e75000930955bdec1f4
YabseraBogale <yabserapython@gmail.com>
2025-05-12 19:04:40 +0300

    added department and office

--- esh/public/js/add_employee.js
+++ esh/public/js/add_employee.js
@@ -1,14 +1,20 @@
 fetch("http://127.0.0.1:8080/office_department_job_title").then((response)=>{
         return response.json()
     }).then(data =>{
-       
-       
+       const job_title=document.getElementById("job_title")
+       const office=document.getElementById("office")
         const department=document.getElementById("department")
-        let option=""
+        let option_office=""
+        let option_department=""
+        let option_job_title=""
         for(let i=0;i<data.length;i++){
-            option+="<option value='"+data[i]+"'>"+data[i]+"</option>"
+            option_office+="<option value='"+data[i][0]+"'>"+data[i]+"</option>"
+            option_department+="<option value='"+data[i][1]+"'>"+data[i]+"</option>"
+            option_job_title+="<option value='"+data[i][2]+"'>"+data[i]+"</option>"
         }
-        department.innerHTML=option
+        office.innerHTML=option_office
+        job_title.innerHTML=option_job_title
+        department.innerHTML=option_department
         
     }).catch(err =>{
         console.error(err)
