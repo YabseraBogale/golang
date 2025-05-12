e1fbcecb5733942e627fdb41d346b9e4481e013c
YabseraBogale <yabserapython@gmail.com>
2025-05-12 18:59:02 +0300

    added department and office

--- esh/public/js/add_employee.js
+++ esh/public/js/add_employee.js
@@ -1,4 +1,4 @@
-fetch("http://127.0.0.1:8080/department").then((response)=>{
+fetch("http://127.0.0.1:8080/office_department_job_title").then((response)=>{
         return response.json()
     }).then(data =>{
        
@@ -13,36 +13,3 @@ fetch("http://127.0.0.1:8080/department").then((response)=>{
     }).catch(err =>{
         console.error(err)
 })
-
-fetch("http://127.0.0.1:8080/office").then((response)=>{
-        return response.json()
-    }).then(data =>{
-       
-       
-        const department=document.getElementById("office")
-        let option=""
-        for(let i=0;i<data.length;i++){
-            option+="<option value='"+data[i]+"'>"+data[i]+"</option>"
-        }
-        department.innerHTML=option
-        
-    }).catch(err =>{
-        console.error(err)
-})
-
-
-fetch("http://127.0.0.1:8080/job_title").then((response)=>{
-        return response.json()
-    }).then(data =>{console.log(data);
-       
-       
-        const job_title=document.getElementById("job_title")
-        let option=""
-        for(let i=0;i<data.length;i++){
-            option+="<option value='"+data[i]+"'>"+data[i]+"</option>"
-        }
-        job_title.innerHTML=option
-        
-    }).catch(err =>{
-        console.error(err)}
-    )
