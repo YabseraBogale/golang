2bb533b0213de5b8bf09a496ef2bb53474caec24
YabseraBogale <yabserapython@gmail.com>
2026-02-27 18:55:15 +0300

    testing

--- sherlock/main.go
+++ sherlock/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+func main() {
+	url := "https://wakatime.com/@"
+	res, err := http.Get(url)
+	if err != nil {
+		log.Println(err)
+	}
+	defer res.Body.Close()
+
+	fmt.Println(res.StatusCode)
 }
