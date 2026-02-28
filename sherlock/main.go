661373c82c2d78c34d37f990c7b7609d27aa09ed
Yabsera Bogale Abate <96526984+YabseraBogale@users.noreply.github.com>
2026-02-28 21:16:39 +0300

    Update main.go

--- sherlock/main.go
+++ sherlock/main.go
@@ -3,5 +3,7 @@ package main
 const data = "https://raw.githubusercontent.com/sherlock-project/sherlock/master/sherlock_project/resources/data.json"
 
 func main() {
-
+d,err:=http.Get(data)
+if err!=nil{
+}
 }
