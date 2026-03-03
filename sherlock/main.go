73cc752d8a0c631c33753304b23d0c850d91f40f
YabseraBogale <yabserapython@gmail.com>
2026-03-03 19:51:58 +0300

    testing the bufio

--- sherlock/main.go
+++ sherlock/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/YabseraBogale/golang/sherlock/site"
@@ -13,47 +15,56 @@ import (
 
 func main() {
 
+	scanner := bufio.NewScanner(os.Stdin)
+
+	scanner.Split(bufio.ScanWords)
 	client := &http.Client{
 		Timeout: 10 * time.Second,
 	}
 
-	resp, err := client.Get(site.Data)
+	for scanner.Scan() {
+		resp, err := client.Get(site.Data)
 
-	if err != nil {
-		log.Println(err)
-	}
-	defer resp.Body.Close()
+		line := scanner.Text()
 
-	if resp.StatusCode != http.StatusOK {
-		log.Fatalln(resp.StatusCode)
-	}
+		fmt.Println("User Name:", line)
 
-	body_byte, err := io.ReadAll(resp.Body)
+		if err != nil {
+			log.Println(err)
+		}
+		defer resp.Body.Close()
 
-	if err != nil {
-		log.Fatalln(err)
-	}
-	var full_map map[string]json.RawMessage
-	err = json.Unmarshal(body_byte, &full_map)
-
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	for key, value := range full_map {
-
-		if key == "$schema" {
-			continue
+		if resp.StatusCode != http.StatusOK {
+			log.Fatalln(resp.StatusCode)
 		}
 
-		var site site.SiteData
+		body_byte, err := io.ReadAll(resp.Body)
 
-		if err := json.Unmarshal(value, &site); err != nil {
-			fmt.Println(err)
-			continue
+		if err != nil {
+			log.Fatalln(err)
 		}
-		fmt.Println(site.URL)
+		var full_map map[string]json.RawMessage
+		err = json.Unmarshal(body_byte, &full_map)
 
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		for key, value := range full_map {
+
+			if key == "$schema" {
+				continue
+			}
+
+			var site site.SiteData
+
+			if err := json.Unmarshal(value, &site); err != nil {
+				fmt.Println(err)
+				continue
+			}
+			fmt.Println(site.URL)
+
+		}
 	}
 
 }
