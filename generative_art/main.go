c79a411e232e17b84779445a15ba647dd65cf312
YabseraBogale <yabserapython@gmail.com>
2026-03-21 10:56:54 +0300

    making

--- generative_art/main.go
+++ generative_art/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+type Game struct{}
+
+func (g *Game) Update() error {
+	return nil
+}
+func (g *Game) Draw(screen *ebiten.Image) {
+
+}
+
+func (g *Game) Layout(outWidth, outHeight int) (screenWidth, screenHeight int) {
+	return 320, 400
+}
+
+func main() {
+	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowTitle("Hello World")
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err)
+	}
 }
