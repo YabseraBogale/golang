40c44eb0ea69fa086ad8fae23d4e7c0eb2b8524d
YabseraBogale <yabserapython@gmail.com>
2026-03-23 17:33:40 +0300

    adding dino sprite

--- dino/main.go
+++ dino/main.go
@@ -1,20 +1,32 @@
 package main
 
+import (
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
 type Game struct {
 }
 
-func (g *Game) Draw() error {
-	return nil
+func (g *Game) Draw(screen *ebiten.Image) {
+
 }
 
 func (g *Game) Layout(out_width, out_height int) (width, height int) {
 	return out_width, out_height
 }
 
-func (g *Game) Update() {
-
+func (g *Game) Update() error {
+	return nil
 }
 
 func main() {
+	game := &Game{}
+	ebiten.SetWindowSize(640, 320)
+	ebiten.SetWindowTitle("Dino")
 
+	if err := ebiten.RunGame(game); err != nil {
+		log.Fatalln(err)
+	}
 }
