f19b6829c99789b8b7172d8c55eeb2c9c198471c
YabseraBogale <yabserapython@gmail.com>
2026-03-24 12:59:10 +0300

    adding dino sprite

--- dino/main.go
+++ dino/main.go
@@ -30,15 +30,35 @@ func MustLodImage(name string) *ebiten.Image {
 
 var player = MustLodImage("assets/player/RedDinosaur1.png")
 var hill_layer_01 = MustLodImage("assets/background/Hills Layer 01.png")
+var hill_layer_02 = MustLodImage("assets/background/Hills Layer 02.png")
+var hill_layer_03 = MustLodImage("assets/background/Hills Layer 03.png")
+var hill_layer_04 = MustLodImage("assets/background/Hills Layer 04.png")
+var hill_layer_05 = MustLodImage("assets/background/Hills Layer 05.png")
 
 func (g *Game) Draw(screen *ebiten.Image) {
 
-	screen.DrawImage(hill_layer_01, nil)
-	op := &ebiten.DrawImageOptions{}
-	op.GeoM.Scale(2, 2)
-	op.GeoM.Translate(150, 200)
+	sw, sh := screen.Bounds().Dx(), screen.Bounds().Dy()
 
-	screen.DrawImage(player, op)
+	layers := []*ebiten.Image{
+		hill_layer_01,
+		hill_layer_02,
+		hill_layer_03,
+		hill_layer_04,
+		hill_layer_05,
+	}
+	for _, layer := range layers {
+		lw, lh := layer.Bounds().Dx(), layer.Bounds().Dy()
+
+		scaleX, scaleY := float64(sw)/float64(lw), float64(sh)/float64(lh)
+		op := &ebiten.DrawImageOptions{}
+		op.GeoM.Scale(scaleX, scaleY)
+		screen.DrawImage(layer, op)
+	}
+	player_opition := &ebiten.DrawImageOptions{}
+	player_opition.GeoM.Scale(2, 2)
+	player_opition.GeoM.Translate(30, 250)
+	screen.DrawImage(player, player_opition)
+
 }
 
 func (g *Game) Layout(out_width, out_height int) (width, height int) {
